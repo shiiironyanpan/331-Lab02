6548690f6dbdec44c0540d6dd69c70bfdf8efef0
zeerowiibu <kunkong007@gmail.com>
2026-08-10 03:10:34 +0700

    Lab04 Checkpoint 1.6

--- SunVue/src/views/event/DetailView.vue
+++ SunVue/src/views/event/DetailView.vue
@@ -1,15 +1,36 @@
 <script setup lang="ts">
 import { toRefs } from 'vue'
 import { type Event } from '@/types'
+import { useMessageStore } from '@/stores/message'
+import { storeToRefs } from 'pinia'
 
 const props = defineProps<{
   event: Event
 }>()
 
 const { event } = toRefs(props)
+const store = useMessageStore()
+const { message } = storeToRefs(store)
 </script>
 
 <template>
+  <div v-if="message" class="flash-message">
+    <h4>{{ message }}</h4>
+  </div>
   <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
   <p>{{ event.description }}</p>
 </template>
+
+<style scoped>
+.flash-message {
+  animation: yellowFade 3s ease-in-out;
+}
+@keyframes yellowFade {
+  from {
+    background-color: yellow;
+  }
+  to {
+    background-color: transparent;
+  }
+}
+</style>
