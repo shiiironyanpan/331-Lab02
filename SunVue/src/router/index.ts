da8b4a57c30a8150798f2af0c517a982eba5a369
zeerowiibu <kunkong007@gmail.com>
2026-08-10 02:38:01 +0700

    Lab03 Checkpoint 5.1

--- SunVue/src/router/index.ts
+++ SunVue/src/router/index.ts
@@ -6,6 +6,7 @@ import EventDetailView from '@/views/event/DetailView.vue'
 import EventRegisterView from '@/views/event/RegisterView.vue'
 import EventEditView from '@/views/event/EditView.vue'
 import EventLayoutView from '@/views/event/LayoutView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -51,6 +52,17 @@ const router = createRouter({
           component: EventEditView
         }
       ]
+    },
+    {
+      path: '/404/:resource',
+      name: '404-resource-view',
+      component: NotFoundView,
+      props: true
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'not-found',
+      component: NotFoundView
     }
   ]
 })
