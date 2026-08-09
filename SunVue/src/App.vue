aabc3d15936a3c6943a6e597e2a96aca14e2d44b
zeerowiibu <kunkong007@gmail.com>
2026-08-10 03:08:01 +0700

    Lab04 1.5 (NOT CHECKPOINT)

--- SunVue/src/App.vue
+++ SunVue/src/App.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { useMessageStore } from '@/stores/message'
+import { storeToRefs } from 'pinia'
+
+const store = useMessageStore()
+const { message } = storeToRefs(store)
 </script>
 
 <template>
 <div id="layout">
     <header>
+      <div id="flashMessage" v-if="message">
+        <h4>{{ message }}</h4>
+      </div>
       <div class="wrapper">
         <nav>
           <RouterLink :to="{ name: 'event-list-view', query: { page: 1 } }">Event</RouterLink> |
@@ -45,5 +53,18 @@ nav a.router-link-exact-active {
 
 h2 {
   font-size: 20px;
+}
+
+@keyframes yellowFade {
+  from {
+    background-color: yellow;
+  }
+  to {
+    background-color: transparent;
+  }
+}
+
+#flashMessage {
+  animation: yellowFade 3s ease-in-out;
 }
 </style>
