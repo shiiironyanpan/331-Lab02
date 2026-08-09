73a49c813f706e7571a9782e9178e2b19f973311
zeerowiibu <kunkong007@gmail.com>
2026-08-10 02:40:07 +0700

    Lab03 Checkpoint 5.2

--- SunVue/src/router/index.ts
+++ SunVue/src/router/index.ts
@@ -7,6 +7,7 @@ import EventRegisterView from '@/views/event/RegisterView.vue'
 import EventEditView from '@/views/event/EditView.vue'
 import EventLayoutView from '@/views/event/LayoutView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
+import NetworkErrorView from '@/views/NetworkErrorView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -63,6 +64,11 @@ const router = createRouter({
       path: '/:catchAll(.*)',
       name: 'not-found',
       component: NotFoundView
+    },
+    {
+      path: '/network-error',
+      name: 'network-error-view',
+      component: NetworkErrorView
     }
   ]
 })
