82fd6ba45b1fb95577c3a8d38b6cc02e9c5a9ead
zeerowiibu <kunkong007@gmail.com>
2026-08-10 03:27:08 +0700

    Lab04 Checkpoint 3.7 (there is no checkpoint 3.6)

--- SunVue/src/views/event/LayoutView.vue
+++ SunVue/src/views/event/LayoutView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { type Event } from '@/types'
+import { type Event } from '@/types' 
 import { useEventStore } from '@/stores/event'
 import { storeToRefs } from 'pinia'
 
