c71f7d4b855424ab4325df45e16c4c06b6ca5211
zeerowiibu <kunkong007@gmail.com>
2026-08-10 15:26:24 +0700

    Lab06 Checkpoint 10.7

--- SunVue/src/App.vue
+++ SunVue/src/App.vue
@@ -2,6 +2,7 @@
 import { RouterLink, RouterView } from 'vue-router'
 import { useMessageStore } from '@/stores/message'
 import { storeToRefs } from 'pinia'
+import { SpeedInsights } from '@vercel/speed-insights/vue'
 
 const store = useMessageStore()
 const { message } = storeToRefs(store)
@@ -23,5 +24,6 @@ const { message } = storeToRefs(store)
       </div>
     </header>
     <RouterView />
+    <SpeedInsights />
 </div>
 </template>
--- SunVue/src/main.ts
+++ SunVue/src/main.ts
@@ -7,10 +7,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import 'nprogress/nprogress.css'
+import { inject } from '@vercel/analytics'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+inject()
 
 app.mount('#app')
